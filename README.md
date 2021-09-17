3e8217d9a53882f6fc747451b7aefc04aab024b9
MathewLister <listermathew0@gmail.com>
2021-09-16 17:09:11 -0700

    reset for scss and custom theme support

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # FitnessLevelCalculator
 
-test
-
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.5.
 
 ## Development server
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/fitness-level-calculator'),
+      dir: require('path').join(__dirname, './coverage/Fitness-Level-Calculator'),
       subdir: '.',
       reporters: [
         { type: 'html' },
